7aff8977c5367192674ae2d37534ec8f6bfcc603
Nicolas Mastroviti <nmastroviti@gmail.com>
2022-06-15 00:11:32 -0300

    chore: update serverless to v3 and migrate configuration - update dependencies - update node to v16

--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,9 +1,9 @@
-import { name, version } from '@/package.json'
+import packageInfo from '@/package.json'
 
 export const sentry = {
   dsn: process.env.SENTRY_DSN,
   environment: process.env.SENTRY_ENVIRONMENT,
-  release: `${name}@${version}`,
+  release: `${packageInfo.name}@${packageInfo.version}`,
   sampleRate: parseFloat(process.env.SENTRY_ERROR_RATE ?? 1),
   tracesSampleRate: parseFloat(process.env.SENTRY_TRANSACTION_RATE ?? 1),
 }
