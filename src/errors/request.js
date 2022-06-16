a4d3054f4c270fbc135bf441f96eed9e430b4113
Nicolas Mastroviti <nmastroviti@gmail.com>
2022-06-15 23:56:28 -0300

    refactor: custom errors

--- src/errors/request.js
+++ src/errors/request.js
@@ -1,63 +1,80 @@
 /* eslint-disable max-classes-per-file */
-
 class RequestError extends Error {
-  constructor(message = 'Bad request.', statusCode = 400) {
+  constructor(message, statusCode, cause) {
     super()
     this.name = 'RequestError'
-    this.message = message
-    this.statusCode = statusCode
+    this.message = message ?? 'Bad request.'
+    this.statusCode = statusCode ?? 400
+    this.cause = cause
     Error.captureStackTrace(this, RequestError)
   }
 }
 
 class InsufficientDataError extends RequestError {
-  constructor(...args) {
-    super(...args)
+  constructor(message, statusCode, cause) {
+    super(message, statusCode, cause)
     this.name = 'InsufficientDataError'
-    this.message = 'Insufficient data to fulfill the request.'
-    this.statusCode = 400
+    this.message = message ?? 'Insufficient data to fulfill the request.'
+    this.statusCode = statusCode ?? 400
     Error.captureStackTrace(this, InsufficientDataError)
   }
 }
 
 class ForbiddenDataError extends RequestError {
-  constructor(...args) {
-    super(...args)
+  constructor(message, statusCode, cause) {
+    super(message, statusCode, cause)
     this.name = 'ForbiddenDataError'
-    this.message = 'Forbidden data in request.'
-    this.statusCode = 400
+    this.message = message ?? 'Forbidden data in request.'
+    this.statusCode = statusCode ?? 400
     Error.captureStackTrace(this, ForbiddenDataError)
   }
 }
 
 class ResourceNotFoundError extends RequestError {
-  constructor(...args) {
-    super(...args)
+  constructor(message, statusCode, cause) {
+    super(message, statusCode, cause)
     this.name = 'ResourceNotFoundError'
-    this.message = 'The requested resource does not exist.'
-    this.statusCode = 404
+    this.message = message ?? 'The requested resource does not exist.'
+    this.statusCode = statusCode ?? 404
     Error.captureStackTrace(this, ResourceNotFoundError)
   }
 }
 
 export class DuplicateResourceError extends RequestError {
-  constructor(message = 'Resource already exists.', statusCode = 409) {
-    super()
+  constructor(message, statusCode, cause) {
+    super(message, statusCode, cause)
     this.name = 'DuplicateResourceError'
-    this.message = message
-    this.statusCode = statusCode
+    this.message = message ?? 'Resource already exists.'
+    this.statusCode = statusCode ?? 409
     Error.captureStackTrace(this, DuplicateResourceError)
   }
 }
 
 class UnsupportedMediaTypeError extends RequestError {
-  constructor(...args) {
-    super(...args)
+  constructor(message, statusCode, cause) {
+    super(message, statusCode, cause)
     this.name = 'UnsupportedMediaTypeError'
-    this.message = 'Media type not supported.'
-    this.statusCode = 415
+    this.message = message ?? 'Media type not supported.'
+    this.statusCode = statusCode ?? 415
     Error.captureStackTrace(this, UnsupportedMediaTypeError)
   }
 }
 
-export { RequestError, ForbiddenDataError, ResourceNotFoundError, InsufficientDataError, UnsupportedMediaTypeError }
+class MethodNotAllowedError extends RequestError {
+  constructor(message, statusCode, cause) {
+    super(message, statusCode, cause)
+    this.name = 'MethodNotAllowedError'
+    this.message = message ?? 'Method not allowed.'
+    this.statusCode = statusCode ?? 405
+    Error.captureStackTrace(this, MethodNotAllowedError)
+  }
+}
+
+export {
+  RequestError,
+  ForbiddenDataError,
+  ResourceNotFoundError,
+  InsufficientDataError,
+  UnsupportedMediaTypeError,
+  MethodNotAllowedError,
+}
