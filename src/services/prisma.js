139355924e545b6188d2e13acf8e4677a0a603c1
Nicolas Mastroviti <nmastroviti@gmail.com>
2022-07-01 02:43:45 -0300

    chore: add support for prisma data proxy

--- src/services/prisma.js
+++ src/services/prisma.js
@@ -1,34 +1,44 @@
-import { PrismaClient } from '@prisma/client'
+/* eslint-disable global-require */
+
+const options = {
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+}
 
 // eslint-disable-next-line import/no-mutable-exports
 let prisma
 
 if (prisma == null) {
-  prisma = new PrismaClient({
-    log: [
-      {
-        emit: 'event',
-        level: 'query',
-      },
-      {
-        emit: 'stdout',
-        level: 'error',
-      },
-      {
-        emit: 'stdout',
-        level: 'info',
-      },
-      {
-        emit: 'stdout',
-        level: 'warn',
-      },
-    ],
-  })
-  prisma.$on('query', ({ query, params, duration }) => {
-    console.log(`Query: ${query}`)
-    console.log(`Params: ${params}`)
-    console.log(`Duration: ${duration}ms`)
-  })
+  if (process.env.PRISMA_DATAPROXY === 'true') {
+    const { PrismaClient } = require('@prisma/client/edge')
+    prisma = new PrismaClient(options)
+  } else {
+    const { PrismaClient } = require('@prisma/client')
+    prisma = new PrismaClient(options)
+  }
+  if (process.env.PRISMA_DATAPROXY !== 'true') {
+    prisma.$on('query', ({ query, params, duration }) => {
+      console.log(`Query: ${query}`)
+      console.log(`Params: ${params}`)
+      console.log(`Duration: ${duration}ms`)
+    })
+  }
 }
 
 export default prisma
