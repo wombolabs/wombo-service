25b5416dfa37e6673f08208cdeae48809e82f6a0
Nicolas Mastroviti <nmastroviti@gmail.com>
2022-08-29 18:58:37 -0300

    fix: resolve issue on session serializer

--- src/services/booking/listPrivateSessions.js
+++ src/services/booking/listPrivateSessions.js
@@ -3,7 +3,7 @@ import pg from '~/services/pg'
 export const listPrivateSessions = async (hidden = false) => {
   const query = {
     name: 'fetch-private-sessions',
-    text: 'SELECT * FROM public."EventType" where hidden = $1::bool',
+    text: 'SELECT * FROM public."EventType" where "hidden" = $1::bool',
     values: [hidden],
   }
   const result = await pg.query(query)
--- src/services/booking/listPrivateSessionsByUserId.js
+++ src/services/booking/listPrivateSessionsByUserId.js
@@ -8,9 +8,9 @@ export const listPrivateSessionsByUserId = async (userId, hidden = false) => {
 
   const query = {
     name: 'fetch-user-private-sessions',
-    text: 'SELECT * FROM public."EventType" where "userId" = $1 and hidden = $2',
+    text: 'SELECT * FROM public."EventType" where "userId" = $1 and "hidden" = $2::bool',
     values: [userId, hidden],
   }
   const result = await pg.query(query)
-  return serializePrivateSessions(result?.rows)
+  return serializePrivateSessions([], result?.rows)
 }
