9e12283b99efb34d61c9bc9e2bccfd7f41f5f8fb
Nicolas Mastroviti <nmastroviti@gmail.com>
2022-09-30 01:13:40 -0300

    feat: add endpoint for league hub enrollment

---
+++ src/handlers/league/hub/enroll.js
@@ -0,0 +1,48 @@
+import { discord as discordConfig } from '~/config'
+import { addGuildMemberRole } from '~/services/discord'
+import { buildHandler } from '~/utils'
+import { authenticationMiddleware } from '~/middlewares'
+import { updateStudentByEmail } from '~/services/students'
+import { InsufficientDataError } from '~/errors'
+
+/**
+ * body {
+ *   valorant {
+ *     trackerUrl: string
+ *     elo: string
+ *     league: string
+ *   }
+ *   faceItId: string
+ *   user {
+ *     birthdate: string yyyy-mm-dd
+ *     displayName: string
+ *     country: string
+ *   }
+ * }
+ */
+const handler = async ({ user, body }, res) => {
+  const { valorant = {}, faceItId, user: player = {} } = body
+
+  const { discord = {} } = user
+  if (discord?.id == null || discord?.accessToken == null || !discord?.scope.includes('guilds.join')) {
+    throw new InsufficientDataError(`Discord required fields are missing for player ${user.email}.`)
+  }
+
+  const roleId = discordConfig.leagueHubRoleIds[valorant?.league?.toLowerCase()]
+  if (!roleId) {
+    throw new InsufficientDataError(`VALORANT required fields are missing for player ${user.email}.`)
+  }
+  await addGuildMemberRole(discord.id, roleId)
+
+  const { displayName, birthdate, country } = player
+  await updateStudentByEmail(user.email, {
+    displayName,
+    metadata: { birthdate, country, valorant, faceItId, leagueHubEnrolled: true },
+  })
+
+  res.json({ enrolled: true })
+}
+
+export const leagueHubEnrollmentHandler = buildHandler('/league/hub/enrollment', 'post', handler, {
+  middlewares: [authenticationMiddleware],
+})
--- src/services/students/updateStudentByEmail.js
+++ src/services/students/updateStudentByEmail.js
@@ -8,9 +8,16 @@ export const updateStudentByEmail = async (email, student) => {
   delete newStudent.email
   delete newStudent.password // TODO implement change password for WP users
 
+  let savedStudent
   if (!R.isEmpty(newStudent.discord)) {
-    const savedStudent = await getStudentByEmail(email)
+    savedStudent = await getStudentByEmail(email)
     newStudent.discord = R.mergeDeepLeft(newStudent.discord, savedStudent.discord)
+  }
+  if (!R.isEmpty(newStudent.metadata)) {
+    if (!savedStudent) {
+      savedStudent = await getStudentByEmail(email)
+    }
+    newStudent.metadata = R.mergeDeepLeft(newStudent.metadata, savedStudent.metadata)
   }
 
   const result = await prisma.student.update({
