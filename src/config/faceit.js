b728653a775850a429162769ed18ac6ffa816968
Nicolas Mastroviti <nmastroviti@gmail.com>
2022-10-27 20:37:41 -0300

    feat: update faceit leagues leaderboard

--- src/config/faceit.js
+++ src/config/faceit.js
@@ -1,9 +1,14 @@
 export const faceit = {
   hubs: [
     {
-      name: 'Wombo Pro Hub - Liga: Free',
-      entityId: '2aa209b7-d626-4eb0-ae45-e92c6515d7ce',
-      id: '633b3c203eff7e27fe121d71',
+      name: 'Wombo Pro Hub - Liga: Pro',
+      entityId: 'c9fc8369-6ac2-457a-ac84-fb0ffaadd950',
+      id: '62df497e40b2f95bc9a997ba',
+    },
+    {
+      name: 'Wombo Pro Hub - Liga: Radiant',
+      entityId: 'aafd77d6-9eab-4eb8-bfc4-fefae2239d89',
+      id: '633b33ee516caf25e2181a61',
     },
     {
       name: 'Wombo Pro Hub - Liga: High',
@@ -11,9 +16,9 @@ export const faceit = {
       id: '633b39b31f2db9285b2729aa',
     },
     {
-      name: 'Wombo Pro Hub - Liga: Radiant',
-      entityId: 'aafd77d6-9eab-4eb8-bfc4-fefae2239d89',
-      id: '633b33ee516caf25e2181a61',
+      name: 'Wombo Pro Hub - Liga: Free',
+      entityId: '2aa209b7-d626-4eb0-ae45-e92c6515d7ce',
+      id: '633b3c203eff7e27fe121d71',
     },
   ],
   apiKey: process.env.FACEIT_API_KEY,
