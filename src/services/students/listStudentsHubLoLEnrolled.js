87e3ef126a5b426ec6466e617c9c0a08a3af9c04
Nicolas Mastroviti <nmastroviti@gmail.com>
2022-12-27 15:52:05 -0300

    refactor: remove unused list  enrolled functions

--- src/services/students/listStudentsHubLoLEnrolled.js
+++
@@ -1,8 +0,0 @@
-import prisma from '~/services/prisma'
-import R from 'ramda'
-
-export const listStudentsHubLoLEnrolled = async () => {
-  const result = await prisma.$queryRaw`
-    SELECT s.* FROM "Student" s WHERE s.metadata->>'leagueHubLoLEnrolled' = 'true'`
-  return !R.isEmpty(result) ? result : []
-}
