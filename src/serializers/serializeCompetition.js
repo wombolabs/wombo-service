27bbeb2a446bb14f0b0f7c3d675f840f47e22057
Nicolas Mastroviti <nmastroviti@gmail.com>
2023-01-03 15:59:37 -0300

    feat: get participants of competition

--- src/serializers/serializeCompetition.js
+++ src/serializers/serializeCompetition.js
@@ -1,4 +1,16 @@
 import R from 'ramda'
 import { DEFAULT_COMPETITION_FIELDS } from '~/services/competitions'
+import { DEFAULT_STUDENT_FIELDS } from '~/services/students'
 
-export const serializeCompetition = R.curry((competition) => R.pick([...DEFAULT_COMPETITION_FIELDS])(competition))
+const studentFields = [...DEFAULT_STUDENT_FIELDS, 'discord']
+
+const serializeParticipants = R.curry((participants) => R.map(R.pick([...studentFields]))(participants))
+
+export const serializeCompetition = R.curry((competition) =>
+  R.pipe(
+    R.pick([...DEFAULT_COMPETITION_FIELDS]),
+    R.evolve({
+      participants: serializeParticipants,
+    })
+  )(competition)
+)
