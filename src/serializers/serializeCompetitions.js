dc5df7dddf65a1f98e010ea5b000302f17e5ed42
Nicolas Mastroviti <nmastroviti@gmail.com>
2023-01-10 15:39:36 -0300

    feat: add flag to get minimal data of participants from competition

--- src/serializers/serializeCompetitions.js
+++ src/serializers/serializeCompetitions.js
@@ -1,4 +1,4 @@
 import R from 'ramda'
 import { serializeCompetition } from './serializeCompetition'
 
-export const serializeCompetitions = R.curry((competitions) => R.map(serializeCompetition)(competitions))
+export const serializeCompetitions = R.curry((competitions) => R.map(serializeCompetition(true))(competitions))
