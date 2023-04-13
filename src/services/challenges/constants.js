a66f0c678591452394d97302eb2b4ae6463736db
Nicolas Mastroviti <nmastroviti@gmail.com>
2023-04-13 02:19:52 -0300

    feat: add list challenges endpoint and serializers

---
+++ src/services/challenges/constants.js
@@ -0,0 +1,12 @@
+export const DEFAULT_CHALLENGE_FIELDS = [
+  'id',
+  'videoGame',
+  'type',
+  'description',
+  'metadata',
+  'betAmount',
+  'prizeWon',
+  'owner',
+  'challenger',
+  'createdAt',
+]
---
+++ src/services/challenges/listChallenges.js
@@ -0,0 +1,30 @@
+import prisma from '~/services/prisma'
+import { notNilNorEmpty } from '~/utils'
+
+export const listChallenges = async (filters = {}) => {
+  const { isActive } = filters
+
+  const where = {}
+  if (typeof isActive === 'boolean') {
+    where.isActive = isActive
+  }
+
+  const query = {
+    include: {
+      owner: true,
+      challenger: true,
+    },
+    orderBy: [
+      {
+        createdAt: 'desc',
+      },
+    ],
+  }
+  if (notNilNorEmpty(where)) {
+    query.where = where
+  }
+
+  const result = await prisma.challenge.findMany(query)
+
+  return result
+}
