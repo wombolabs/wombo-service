32c4bb561845718ca0b10f217912bf002434fd1a
Nicolas Mastroviti <nmastroviti@gmail.com>
2023-05-04 23:08:50 -0300

    refactor: get wallet from student

--- src/handlers/student/get.js
+++ src/handlers/student/get.js
@@ -1,10 +1,10 @@
 import { buildHandler } from '~/utils'
-import { serializePublicStudent } from '~/serializers'
+import { serializePublicStudent, serializeStudent } from '~/serializers'
 import { getStudentByUsername } from '~/services/students'
 
 const handler = async ({ params: { username }, query }, res) => {
   const result = await getStudentByUsername(username, query)
-  return res.json(serializePublicStudent(result))
+  return res.json(query.withWallet ? serializeStudent(result) : serializePublicStudent(result))
 }
 
 export const getStudentHandler = buildHandler('/students/:username', 'get', handler)
--- src/services/students/getStudentByUsername.js
+++ src/services/students/getStudentByUsername.js
@@ -1,12 +1,23 @@
 import prisma from '~/services/prisma'
 import { ResourceNotFoundError, InsufficientDataError } from '~/errors'
+import { notNilNorEmpty } from '~/utils/notNilNorEmpty'
 
-export const getStudentByUsername = async (username) => {
+export const getStudentByUsername = async (username, filters = {}) => {
   if (!username) {
     throw new InsufficientDataError('Username field is required.')
   }
 
+  const { withWallet } = filters
+
+  const include = {}
+  if (withWallet) {
+    include.wallet = true
+  }
+
   const query = { where: { username: { equals: username, mode: 'insensitive' }, isActive: true } }
+  if (notNilNorEmpty(include)) {
+    query.include = include
+  }
 
   const result = await prisma.student.findFirst(query)
 
