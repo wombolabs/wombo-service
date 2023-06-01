f02a386048f0d8e3004aef389056e9553f8f5917
Nicolas Mastroviti <nmastroviti@gmail.com>
2023-06-01 00:40:37 -0300

    feat: add sort by mmr on competition leaderboards

--- src/handlers/competition/listLeaderboards.js
+++ src/handlers/competition/listLeaderboards.js
@@ -1,13 +1,10 @@
 import R from 'ramda'
 import { getCompetitionByCodename } from '~/services/competitions'
 import { listChannelsLeaderboard } from '~/services/providers/neatqueue'
-import { getStudentByDiscordId } from '~/services/students'
-import { getVideoGameById } from '~/services/videoGames'
 import { buildHandler, notNilNorEmpty } from '~/utils'
 
 const handler = async ({ params: { codename } }, res) => {
   let result = []
-  let videoGame = null
 
   const competition = await getCompetitionByCodename(codename, { isActive: true })
 
@@ -18,30 +15,8 @@ const handler = async ({ params: { codename } }, res) => {
       const { discordGuild, apiKey, channels } = leaderboard
       try {
         result = await listChannelsLeaderboard(discordGuild, apiKey, channels)
-
-        videoGame = await getVideoGameById(competition.videoGame)
       } catch (err) {
         result = []
-      }
-
-      if (notNilNorEmpty(videoGame)) {
-        const resultPromises = R.map(async (lb) => {
-          const itemsPromises = R.map(async (player) => {
-            const student = await getStudentByDiscordId(player.id)
-
-            return {
-              country: student?.metadata?.profile?.country || '',
-              teamName: student?.metadata[videoGame?.codename]?.teamName || '',
-              ...player,
-              username: student?.username || player.username,
-            }
-          })(lb.items)
-
-          const items = await Promise.all(itemsPromises)
-          return { name: lb.name, items }
-        })(result)
-
-        result = await Promise.all(resultPromises)
       }
     }
   }
