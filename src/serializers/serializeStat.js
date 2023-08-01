ec04ef68164c72550b35d72720cd82fabd76d015
Nicolas Mastroviti <nmastroviti@gmail.com>
2023-08-01 19:43:55 -0300

    refactor: add student metadata to list stats service

--- src/serializers/serializeStat.js
+++ src/serializers/serializeStat.js
@@ -3,14 +3,17 @@ import { DEFAULT_STAT_FIELDS } from '~/services/stats'
 
 export const serializeStat = R.curry((stats) =>
   R.pipe(
-    R.assoc('username', stats.owner?.username),
+    R.assoc('student', {
+      username: stats.owner?.username,
+      country: stats.owner?.metadata?.profile?.country,
+    }),
     R.assoc('totalMatches', stats.matchesWon + stats.matchesDraw + stats.matchesLost),
     R.assoc('winRate', Math.round((stats.matchesWon / (stats.matchesWon + stats.matchesLost)) * 100)),
     R.assoc(
       'avgOpponentsRating',
-      Math.round(stats.accOpponentsRating / (stats.matchesWon + stats.matchesDraw + stats.matchesLost))
+      Math.round(stats.accOpponentsRating / (stats.matchesWon + stats.matchesDraw + stats.matchesLost)),
     ),
     R.assoc('matchesGoalsDifference', stats.matchesGoalsFor - stats.matchesGoalsAgainst),
-    R.pick([...DEFAULT_STAT_FIELDS])
-  )(stats)
+    R.pick([...DEFAULT_STAT_FIELDS]),
+  )(stats),
 )
