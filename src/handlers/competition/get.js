48430f02c0937e85fdcaf5b363bcd961dd770f27
Nicolas Mastroviti <nmastroviti@gmail.com>
2023-08-09 22:35:01 -0300

    refactor: modify competition serializer - modify include participants on competition query

--- src/handlers/competition/get.js
+++ src/handlers/competition/get.js
@@ -4,7 +4,7 @@ import { serializeCompetition } from '~/serializers'
 
 const handler = async ({ params: { codename }, query }, res) => {
   const result = await getCompetitionByCodename(codename, query)
-  return res.json(serializeCompetition(query?.withMinimalDataParticipants)(result))
+  return res.json(serializeCompetition(result))
 }
 
 export const getCompetitionHandler = buildHandler('/competitions/:codename', 'get', handler)
--- src/serializers/serializeCompetition.js
+++ src/serializers/serializeCompetition.js
@@ -1,47 +1,31 @@
 import R from 'ramda'
 import { DEFAULT_COMPETITION_FIELDS } from '~/services/competitions'
-import { DEFAULT_STUDENT_FIELDS } from '~/services/students'
 import { serializeChallenges } from './serializeChallenges'
-
-const studentFields = [...DEFAULT_STUDENT_FIELDS, 'discord']
-
-const serializeParticipantDiscord = R.curry((discord) => R.pick(['username', 'discriminator'])(discord))
 
 const serializeParticipants = R.curry((participants) =>
   R.map(
     R.pipe(
-      R.pick([...studentFields]),
-      R.evolve({
-        discord: serializeParticipantDiscord,
-      })
-    )
-  )(participants)
-)
-
-const serializeMinimalDataParticipants = R.curry((participants) =>
-  R.map(
-    R.pipe(
-      R.pick(['id', 'username', 'metadata']),
+      R.pick(['id', 'username', 'metadata', 'stat']),
       R.evolve({
         metadata: R.curry((metadata) =>
           R.pipe(
-            R.pick(R.keys(metadata)),
+            R.pick(['profile']),
             R.evolve({
               profile: R.curry((profile) => R.pick(['picture', 'country'])(profile)),
-            })
-          )(metadata)
+            }),
+          )(metadata),
         ),
-      })
-    )
-  )(participants)
+      }),
+    ),
+  )(participants),
 )
 
-export const serializeCompetition = R.curry((withMinimalDataParticipants, competition) =>
+export const serializeCompetition = R.curry((competition) =>
   R.pipe(
     R.pick([...DEFAULT_COMPETITION_FIELDS]),
     R.evolve({
-      participants: withMinimalDataParticipants ? serializeMinimalDataParticipants : serializeParticipants,
+      participants: serializeParticipants,
       challenges: serializeChallenges,
-    })
-  )(competition)
+    }),
+  )(competition),
 )
