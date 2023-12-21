67765d569a443863a41361d9378aac157002a84e
Nicolas Mastroviti <nmastroviti@gmail.com>
2023-12-21 06:22:11 -0300

    feat: add query filter on list stats endpoint

--- src/services/stats/listStats.js
+++ src/services/stats/listStats.js
@@ -1,6 +1,14 @@
 import prisma from '~/services/prisma'
+import { notNilNorEmpty } from '~/utils'
 
-export const listStats = async () => {
+export const listStats = async (filters) => {
+  const { cmsVideoGameHandleId } = filters // list stats from cmsVideoGameHandleId
+
+  const where = {}
+  if (notNilNorEmpty(cmsVideoGameHandleId)) {
+    where.cmsVideoGameHandleId = { equals: cmsVideoGameHandleId }
+  }
+
   const query = {
     include: {
       owner: {
@@ -15,6 +23,9 @@ export const listStats = async () => {
         rating: 'desc',
       },
     ],
+  }
+  if (notNilNorEmpty(where)) {
+    query.where = where
   }
 
   const result = await prisma.stat.findMany(query)
