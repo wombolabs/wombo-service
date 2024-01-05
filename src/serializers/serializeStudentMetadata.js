1bce241cad857734b10914d4098c42778c02702c
Nicolas Mastroviti <nmastroviti@gmail.com>
2024-01-05 19:36:05 -0300

    refactor: update serializer student

---
+++ src/serializers/serializeStudentMetadata.js
@@ -0,0 +1,25 @@
+import R from 'ramda'
+
+const serializeMetadataProfile = R.curry((profile) =>
+  R.unless(
+    R.isNil,
+    R.pipe(
+      R.pick(['availability', 'birthdate', 'cellphone', 'country', 'picture', 'socialMedias', 'geoInfo']),
+      R.evolve({
+        geoInfo: R.curry((geoInfo) => R.unless(R.isNil, R.pick(['countryCode', 'countryName']))(geoInfo)),
+      }),
+    ),
+  )(profile),
+)
+
+export const serializeStudentMetadata = R.curry((metadata) =>
+  R.unless(
+    R.isNil,
+    R.pipe(
+      R.pick(['profile']),
+      R.evolve({
+        profile: serializeMetadataProfile,
+      }),
+    ),
+  )(metadata),
+)
--- src/serializers/serializeStudentPublic.js
+++ src/serializers/serializeStudentPublic.js
@@ -1,10 +1,11 @@
 import R from 'ramda'
+import { serializeStudentMetadata } from './serializeStudentMetadata'
 
 export const serializeStudentPublic = R.curry((student) =>
   R.pipe(
     R.pick(['id', 'email', 'username', 'displayName', 'metadata', 'stats']),
     R.evolve({
-      metadata: R.curry((metadata) => R.pick(['profile'])(metadata)),
+      metadata: serializeStudentMetadata,
       stats: R.curry((stats) => R.unless(R.isNil, R.map(R.pick(['rating', 'cmsVideoGameHandleId'])))(stats)),
     }),
   )(student),
