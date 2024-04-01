9838ac7fe94a6f424572edb23dd02c5cc05ea08c
Nicolas Mastroviti <nmastroviti@gmail.com>
2024-04-01 19:49:05 -0300

    refactor: add types to challenge and modify owner field of challenge to be an optional

---
+++ prisma/migrations/20240401230219_student_oauth_provider/migration.sql
@@ -0,0 +1,12 @@
+-- AlterEnum
+ALTER TYPE "ChallengeType" ADD VALUE 'anonymous_player_vs_player';
+ALTER TYPE "ChallengeType" ADD VALUE 'matchmaking_player_vs_player';
+
+-- DropForeignKey
+ALTER TABLE "Challenge" DROP CONSTRAINT "Challenge_ownerId_fkey";
+
+-- AlterTable
+ALTER TABLE "Challenge" ALTER COLUMN "ownerId" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Challenge" ADD CONSTRAINT "Challenge_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Student"("id") ON DELETE SET NULL ON UPDATE CASCADE;
