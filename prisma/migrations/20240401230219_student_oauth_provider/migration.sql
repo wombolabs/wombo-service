fd5ca9901226ecc95f0bce54d4d3dbfcb71d5d6e
Nicolas Mastroviti <nmastroviti@gmail.com>
2024-04-09 12:31:47 -0300

    chore: comment sql migration (error on production)

--- prisma/migrations/20240401230219_student_oauth_provider/migration.sql
+++ prisma/migrations/20240401230219_student_oauth_provider/migration.sql
@@ -3,10 +3,10 @@ ALTER TYPE "ChallengeType" ADD VALUE 'anonymous_player_vs_player';
 ALTER TYPE "ChallengeType" ADD VALUE 'matchmaking_player_vs_player';
 
 -- DropForeignKey
-ALTER TABLE "Challenge" DROP CONSTRAINT "Challenge_ownerId_fkey";
+--ALTER TABLE "Challenge" DROP CONSTRAINT "Challenge_ownerId_fkey";
 
 -- AlterTable
-ALTER TABLE "Challenge" ALTER COLUMN "ownerId" DROP NOT NULL;
+--ALTER TABLE "Challenge" ALTER COLUMN "ownerId" DROP NOT NULL;
 
 -- AddForeignKey
-ALTER TABLE "Challenge" ADD CONSTRAINT "Challenge_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Student"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+--ALTER TABLE "Challenge" ADD CONSTRAINT "Challenge_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "Student"("id") ON DELETE SET NULL ON UPDATE CASCADE;
